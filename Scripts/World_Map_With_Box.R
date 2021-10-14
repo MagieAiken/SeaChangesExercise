bcd1753f3283a05ba5c702d9a1f5eede50191c3f
Liz Quinlan <60393295+LizQuinlan@users.noreply.github.com>
2021-10-14 10:32:21 +0100

    Update and rename World_Map_With_Box.R to Scripts/World_Map_With_Box.R

--- Scripts/World_Map_With_Box.R
+++ Scripts/World_Map_With_Box.R
@@ -1,3 +1,14 @@
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+library(rgeos)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+
 world<- ne_countries(scale="medium", returnclass = "sf")
 world_map<-ggplot(data=world) + geom_sf(color="grey", fill="grey")+ theme(plot.margin=unit(c(0,0,0,0), "cm")) + coord_sf(xlim= c(-98, 12), ylim= c(-0.9,68)) + geom_rect(color="red", fill= NA, xmax= c(11), xmin = c(-5), ymax= c(48), ymin= c(60)) + theme(axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank())
 world_map
